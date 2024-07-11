129e5466d55aa1c2cf45024f32fe2b941ce2711c
bspowell <82974206+bspowell@users.noreply.github.com>
2024-07-10 18:43:19 -0700

    fix: change poetry run_dev script to work with scripts.py function
    Co-authored-by: Garrett Cochran <garrettjcochran@gmail.com>
    Co-authored-by:  <james.drabinsky@gmail.com>

--- scripts.py
+++ scripts.py
@@ -10,3 +10,7 @@ def start_db():
 def build_image():
     result = subprocess.run(["sh", "database/scripts/build-image.sh"], check=True)
     sys.exit(result.returncode)
+
+def run_dev():
+    result = subprocess.run(["python3", "app/main.py"], check=True)
+    sys.exit(result.returncode)
