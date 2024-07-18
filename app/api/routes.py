7bab3e53896a6276c83ff63d3ef2e586b37e3308
kuanchiliao1 <kuanchiliao@gmail.com>
2024-07-18 15:31:08 -0700

    security: add function for detecting possibly malicious sql
    Merged with main
    
    Co-authored-by: jamesdrabinsky <jamesdrabinsky@users.noreply.github.com>

--- app/api/routes.py
+++ app/api/routes.py
@@ -19,14 +19,17 @@ from app.utils.helpers import (
     get_stream_arn,
     )
 
-api = Blueprint('main', __name__)
+api = Blueprint("main", __name__)
 global_boto3_session = None
+
 
 @api.route("/databases", methods=["GET"])
 def get_databases():
     try:
         client = current_app.get_ch_client()
-        print(f"in database route, client, from current_app.get_ch_client(), is: {client}")
+        print(
+            f"in database route, client, from current_app.get_ch_client(), is: {client}"
+        )
 
         db_table_map = {}
         for db in get_db_names(client):
@@ -57,10 +60,11 @@ def query():
         return jsonify({"error": str(e)}), 400
 
 
+
 @api.route("/authenticate", methods=["POST"])
 def authenticate():
     global global_boto3_session
-    
+
     try:
         data = request.json
         access_key = data.get("accessKey")
@@ -69,31 +73,32 @@ def authenticate():
         global_boto3_session = boto3.Session(
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key,
-            region_name='us-west-1'
+            region_name="us-west-1",
         )
-        kinesis_client = global_boto3_session.client('kinesis')
-        
-        response = kinesis_client.list_streams()
-        stream_names = response.get('StreamNames', [])
+        kinesis_client = global_boto3_session.client("kinesis")
 
-        return jsonify({
-            "authenticated": True,
-            "streamNames": stream_names
-        })
+        response = kinesis_client.list_streams()
+        stream_names = response.get("StreamNames", [])
+
+        return jsonify({"authenticated": True, "streamNames": stream_names})
     except Exception as e:
         return jsonify({"Authentication Route Error": str(e)}), 400
+
 
 @api.route("/kinesis-sample", methods=["POST"])
 def kinesis_sample():
     client = current_app.get_ch_client()
     try:
         if global_boto3_session is None:
-            return jsonify({'Authentication Error': 'User had not been authenticated'}), 401
+            return (
+                jsonify({"Authentication Error": "User had not been authenticated"}),
+                401,
+            )
 
         data = request.json
         stream_name = data.get("streamName")
 
-        kinesis_client = global_boto3_session.client('kinesis')
+        kinesis_client = global_boto3_session.client("kinesis")
         shard_iterator = kinesis_client.get_shard_iterator(
             StreamName=stream_name,
             ShardId='shardId-000000000000',  # Assume single shard for simplicity
@@ -125,23 +130,24 @@ def kinesis_sample():
 
                 schemaArray = []
                 for row in res.result_rows:
-                    schema = {
-                        'name': row[0],
-                        'type': row[1]
-                    }
+                    schema = {"name": row[0], "type": row[1]}
                     schemaArray.append(schema)
 
-                return jsonify({"sampleEvent": json.loads(record_data), "inferredSchema": schemaArray})
+                return jsonify(
+                    {
+                        "sampleEvent": json.loads(record_data),
+                        "inferredSchema": schemaArray,
+                    }
+                )
 
             count += 1
-            sleep(1/times_per_second)
-        
-        # connected = connect_to_stream(client, kinesis_client, shard_iterator)
+            sleep(1 / times_per_second)
 
         return jsonify({"Unsuccessful": "could not find any records"})
-      
+
     except Exception as e:
         return jsonify({"Kinesis Sample Route Error": str(e)}), 400
+
 
 @api.route("/create-table", methods=["POST"])
 def create_table():
@@ -149,9 +155,14 @@ def create_table():
         client = current_app.get_ch_client()
 
         if global_boto3_session is None:
-            return jsonify({'Authentication Error': 'User had not been authenticated'}), 401
-        
-        stream_name, table_name, database_name, schema = destructure_create_table_request(request)
+            return (
+                jsonify({"Authentication Error": "User had not been authenticated"}),
+                401,
+            )
+
+        stream_name, table_name, database_name, schema = (
+            destructure_create_table_request(request)
+        )
 
         # validate_schema(schema)
         
@@ -165,18 +176,24 @@ def create_table():
         columns = ", ".join([f'{col["name"]} {col["type"]}' for col in schema])
 
         # Primary key is set to first column as default
-        create_table_query = f"CREATE TABLE {database_name}.{table_name} "\
-                              f"({columns}"\
-                              f") ENGINE = MergeTree()"\
-                              f" PRIMARY KEY {schema[0]["name"]}"
-        
+        create_table_query = f"""
+            CREATE TABLE {database_name}.{table_name} (
+                {columns}
+            ) ENGINE = MergeTree()
+            PRIMARY KEY {schema[0]['name']}
+        """
+
         query = create_table_query.strip()
-        print('create table query: ', query)
+
+        if is_sql_injection(query, True):
+            return jsonify({"Error": "SQL injection detected"})
+
+        print("create table query: ", query)
         client.command(query)
 
         # TODO: Add Lambda Connection here to add a trigger for the Kinesis stream
         # Don't do it within other routes as it will stream events too early causing and error
-        
+
         stream_arn = get_stream_arn(global_boto3_session, stream_name)
         table_id = get_table_id(client, table_name)
         add_table_stream_dynamodb(global_boto3_session, stream_arn, table_id)
--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,20 +1,17 @@
-import json
+import re
 from flask import jsonify
-from time import sleep
 from boto3.dynamodb.conditions import Key
+
 
 # get-databases route
 def get_db_names(client):
-    return [
-        db["name"] for db in client.query("SHOW DATABASES").named_results()
-    ]
+    return [db["name"] for db in client.query("SHOW DATABASES").named_results()]
+
 
 def get_tables_in_db(client, db_name):
     return [
         table["name"]
-        for table in client.query(
-            f"SHOW TABLES FROM {db_name}"
-        ).named_results()
+        for table in client.query(f"SHOW TABLES FROM {db_name}").named_results()
     ]
 
 
@@ -37,6 +34,7 @@ def destructure_query_request(request):
     
     return query_string, page, page_size, offset
 
+
 def create_paginated_query(query_string, page_size, offset):
     paginated_query = f"{query_string} LIMIT {page_size} OFFSET {offset}"
     return paginated_query
@@ -46,53 +44,79 @@ def create_paginated_query(query_string, page_size, offset):
 
 # create-table route
 
+
 def destructure_create_table_request(request):
     data = request.json
     stream_name = data["streamName"]
     table_name = data["tableName"]
-    database_name = data.get('databaseName', 'default')
+    database_name = data.get("databaseName", "default")
     schema = data["schema"]
     return stream_name, table_name, database_name, schema
 
 
+
+def is_sql_injection(query, create_table=False):
+    # List of dangerous SQL keywords
+    dangerous_keywords = [
+        "DROP",
+        "DELETE",
+        "TRUNCATE",
+        "ALTER",
+        "INSERT",
+        "EXEC",
+        "EXECUTE",
+        "UPDATE",
+        "UNION",
+        "--",
+    ]
+
+    if create_table:
+        dangerous_keywords.append(';')
+
+    # Create a regex pattern
+    pattern = (
+        r"\b(" + "|".join(re.escape(keyword) for keyword in dangerous_keywords) + r")\b"
+    )
+
+    # Check if any of the dangerous keywords are in the query
+    if re.search(pattern, query, re.IGNORECASE):
+        return True
+    return False
+
+
 def get_stream_arn(global_boto3_session, stream_name):
-    kinesis_client = global_boto3_session.client('kinesis')
+    kinesis_client = global_boto3_session.client("kinesis")
     stream_description = kinesis_client.describe_stream(StreamName=stream_name)
-    stream_arn = stream_description['StreamDescription']['StreamARN']
+    stream_arn = stream_description["StreamDescription"]["StreamARN"]
     return stream_arn
 
+
 def get_table_id(client, table_name):
-    res = client.query(f"""
+    res = client.query(
+        f"""
         SELECT uuid
         FROM system.tables
         WHERE database = 'default'
         AND name = '{table_name}'
-        """)
+        """
+    )
     return res.first_row[0]
-        
+
 
 def add_table_stream_dynamodb(session, stream_arn, ch_table_id):
-    dynamo_client = session.resource('dynamodb')
-    dynamo_table = dynamo_client.Table('tables_streams')
+    dynamo_client = session.resource("dynamodb")
+    dynamo_table = dynamo_client.Table("tables_streams")
 
     response = dynamo_table.query(
-        KeyConditionExpression=Key('stream_id').eq(stream_arn)
+        KeyConditionExpression=Key("stream_id").eq(stream_arn)
     )
-    
+
     # TODO: this delete if exists is not working
-    if len(response['Items']) == 1:
+    if len(response["Items"]) == 1:
         # If the item exists, delete it first
         dynamo_table.delete_item(
-            Key={
-                "stream_id": stream_arn,
-                "table_id": response['Items'][0]['table_id']
-            }
+            Key={"stream_id": stream_arn, "table_id": response["Items"][0]["table_id"]}
         )
         print(f"Existing entry for stream_id {stream_arn} deleted.")
 
-    dynamo_table.put_item(
-        Item={
-            "stream_id": stream_arn,
-            "table_id": str(ch_table_id)
-        }
-    )
+    dynamo_table.put_item(Item={"stream_id": stream_arn, "table_id": str(ch_table_id)})
